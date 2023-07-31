89e5d09e7df4b54eea3cd49bb2dc84189d5fe017
Lucas Felix <lucariozin.net@gmail.com>
2023-07-31 18:34:54 -0300

    feat: implementing 'saveUserTimeIntervals' on the server side

--- src/pages/api/users/time-intervals/index.api.ts
+++ src/pages/api/users/time-intervals/index.api.ts
@@ -1,40 +1,34 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 
-// import { prisma } from '@/services/prisma'
+import zod from 'zod'
 
-import type { TimeInterval } from '@/services/api/api.types'
 import { getServerSession } from 'next-auth'
+
+import { prisma } from '@/services/prisma'
+
 import { buildNextAuthOptions } from '../../auth/[...nextauth].api'
 
-type ValidateTimeIntervalsInput = (timeIntervals?: TimeInterval[]) => boolean
+const timeIntervalsValidationSchema = zod
+  .object({
+    day: zod.string(),
+    startTimeInMinutes: zod.number(),
+    endTimeInMinutes: zod.number(),
+  })
+  .array()
+  .refine((timeIntervals) => !!timeIntervals)
+  .refine((timeIntervals) => {
+    return timeIntervals.every(
+      (timeInterval) =>
+        timeInterval.day &&
+        timeInterval.startTimeInMinutes &&
+        timeInterval.endTimeInMinutes,
+    )
+  })
 
-const validateTimeIntervalsInput: ValidateTimeIntervalsInput = (
-  timeIntervals,
-) => {
-  if (!timeIntervals) return false
-
-  if (timeIntervals.length === 0) return false
-
-  return timeIntervals.every(
-    (timeInterval) =>
-      timeInterval.day &&
-      timeInterval.startTimeInMinutes &&
-      timeInterval.endTimeInMinutes,
-  )
-}
+type TimeIntervals = zod.infer<typeof timeIntervalsValidationSchema>
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   if (req.method !== 'POST') return res.status(405).end()
-
-  const timeIntervals: TimeInterval[] = req.body.timeIntervals
-
-  const timeIntervalsAreValid = validateTimeIntervalsInput(timeIntervals)
-
-  if (!timeIntervalsAreValid) {
-    return res
-      .status(400)
-      .json({ message: 'User time intervals are not valid' })
-  }
 
   const session = await getServerSession(
     req,
@@ -42,35 +36,34 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
     buildNextAuthOptions(req, res),
   )
 
-  if (!session) {
-    return res.status(400).json({ message: 'User is not logged in' })
+  const userId = session?.user?.id
+
+  if (!session || !userId) return res.status(401).end()
+
+  const timeIntervals: TimeIntervals = req.body.timeIntervals
+
+  const { success } = timeIntervalsValidationSchema.safeParse(timeIntervals)
+
+  if (!success) {
+    return res
+      .status(400)
+      .json({ message: 'User time intervals are not valid' })
   }
 
-  // const userId = (session.user as { id?: string })?.id
+  await Promise.all(
+    timeIntervals.map((timeInterval) =>
+      prisma.userTimeInterval.create({
+        data: {
+          week_day: Number(timeInterval.day),
+          start_time_in_minutes: timeInterval.startTimeInMinutes,
+          end_time_in_minutes: timeInterval.endTimeInMinutes,
+          user_id: userId,
+        },
+      }),
+    ),
+  )
 
-  return res.status(200).json({ session })
-
-  // const userId = session.user.id
-
-  // if (!userId) return res.status(400).json({ message: 'User id not found' })
-
-  // const user = await prisma.user.findUnique({ where: { id: userId } })
-
-  // if (!user) return res.status(400).json({ message: 'User not found' })
-
-  // const mappedTimeIntervals = timeIntervals.map((timeInterval) => ({
-  //   week_day: timeInterval.day,
-  //   start_time_in_minutes: timeInterval.startTimeInMinutes,
-  //   end_time_in_minutes: timeInterval.endTimeInMinutes,
-  //   user_id: userId,
-  // }))
-
-  // await prisma.userTimeInterval.createMany({
-  //   data: mappedTimeIntervals,
-  //   skipDuplicates: true,
-  // })
-
-  // return res.status(201).end()
+  return res.status(201).end()
 }
 
 export default handler
